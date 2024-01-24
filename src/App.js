c04cee069d3cd88ad6ce35e053b2805f1ccf6216
palremitpr <chooxmbamalu@gmail.com>
2024-01-24 21:49:54 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import ContactUs from "./components/ContactUs";
+import Layout from "./components/Layout";
+import { Route, Routes } from "react-router-dom";
+import BuyToken from "./components/BuyToken";
+import AuditsKyc from "./components/AuditsKyc";
+
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/buy-token" element={ <BuyToken/>} />
+        <Route path="/audits-kyc" element={ <AuditsKyc/>} />        
+       
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
